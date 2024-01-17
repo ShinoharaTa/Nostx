a322ea81d8f459c0a6e737d6f34dffb0483054b0
tshinohara <takuma1201@gmail.com>
2024-01-17 21:51:12 +0900

    Nosvelte からの卒業

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,63 +1,35 @@
 <script lang="ts">
-  import { Metadata, NostrApp, Text } from "nosvelte";
-  import "websocket-polyfill";
+  import { getSingleItem } from "$lib/nostr";
 
-  const relays = ["wss://relay.damus.io", "wss://relay.snort.social"];
   export let id: string = "";
+  let metadata: { [key: string]: any } | null = null;
+  const getItem = async () => {
+    const data = await getSingleItem({ kind: 0, author: id });
+    if (data) metadata = JSON.parse(data.content);
+  };
+  getItem();
 </script>
 
-<NostrApp {relays}>
-  <Metadata queryKey={["metadata", id]} pubkey={id} let:metadata>
-    <div class="item">
-      <!-- <div
-        class="background"
-        style={`background-image: url(${JSON.parse(metadata.content).banner});`}
-      />
-      <div class="overlay" /> -->
-      <div class="d-flex mt-4">
-        <img
-          src={JSON.parse(metadata.content).picture}
-          alt=""
-          class="picture"
-        />
-        <div>
-          <div class="text-break">
-            {JSON.parse(metadata.content).display_name}
-          </div>
-          <div class="text-break">
-            @{JSON.parse(metadata.content).name}
-          </div>
+{#if metadata}
+  <div class="item">
+    <div class="d-flex mt-4">
+      <img src={metadata.picture} alt="" class="picture" />
+      <div>
+        <div class="text-break">
+          {metadata.display_name}
+        </div>
+        <div class="text-break">
+          @{metadata.name}
         </div>
       </div>
-      <div class="mt-3 about">
-        {JSON.parse(metadata.content).about}
-      </div>
     </div>
-  </Metadata>
-</NostrApp>
+    <div class="mt-3 about">
+      {metadata.about}
+    </div>
+  </div>
+{/if}
 
 <style>
-  /* .background {
-    content: "";
-    background-repeat: no-repeat;
-    background-size: cover;
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 120px;
-    width: 100%;
-  }
-
-  .overlay {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 121px;
-    background-image: linear-gradient(180deg, transparent 0 60%, #fff 100%);
-  } */
-
   .picture {
     border-radius: 50%;
     overflow: hidden;
